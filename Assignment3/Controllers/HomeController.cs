9d11c1fc8fc44eee36b266608ed72ffb1bc0ef2a
annikadeon <66037631+annikadeon@users.noreply.github.com>
2021-02-03 16:32:19 -0700

    Add project files.

---
+++ Assignment3/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using Assignment3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MyPodcasts()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult MovieForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MovieForm(ApplicationResponse applicationResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                TempStorage.AddApplication(applicationResponse);
+                return View("Confirmation", applicationResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public IActionResult MoviesList()
+        {
+            return View(TempStorage.Applications);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
